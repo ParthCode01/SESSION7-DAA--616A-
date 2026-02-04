8ae690189cfd662fdf7a0745d050839ed2a1ff7e
ParthCode01 <parthcsework@gmail.com>
2026-02-04 12:00:43 +0530

    replaced file

--- experiment3/SlidingWin.cpp
+++ experiment3/SlidingWin.cpp
@@ -1,36 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        vector<int> a(n);
-        for (int i = 0; i < n; i++) cin >> a[i];
-
-        vector<bool> possible(n + 1, false);
-
-        for (int i = 0; i < n; i++) {
-            int sum = a[i];
-            for (int j = i + 1; j < n; j++) {
-                sum += a[j];
-                if (sum > n) break;
-                possible[sum] = true;
-            }
-        }
-
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            if (possible[a[i]]) ans++;
-        }
-
-        cout << ans << "\n";
+    int N;
+    cin >> N;
+    vector<char> arr(N);
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
     }
+    unordered_map<int, int> mp;
+    int sum = 0, maxLen = 0;
+    mp[0] = -1;  
 
+    for (int i = 0; i < N; i++) {
+        if (arr[i] == 'P')
+            sum += 1;
+        else
+            sum -= 1;
+        if (mp.find(sum) != mp.end()) {
+            int len = i - mp[sum];
+            maxLen = max(maxLen, len);
+        } else {
+            mp[sum] = i;  
+        }
+    }
+    cout << maxLen;
     return 0;
 }
